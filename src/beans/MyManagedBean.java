c9c12b1fe145c150447a41b12f1d0adba38541f0
Xypherdon <rosian.filip@gmail.com>
2018-12-13 15:49:26 +0200

    done

---
+++ src/beans/MyManagedBean.java
@@ -0,0 +1,35 @@
+package beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class MyManagedBean {
+
+	private String out = "";
+
+	public String getOut() { return out; }
+
+	public void setOut(String out) { this.out = out; }
+
+	public void up(){
+		out = out.concat("\nUp!");
+	}
+
+	public void down(){
+		out = out.concat("\nDown!");
+	}
+
+	public void left(){
+		out = out.concat("\nLeft!");
+	}
+
+	public void right(){
+		out = out.concat("\nRight!");
+	}
+
+	public void keypress(){
+		out = out.concat("\nKey!");
+	}
+}
